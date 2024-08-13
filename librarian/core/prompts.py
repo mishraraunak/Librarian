ca006d067346e0514aa2a8a47b17a1ebecce0530
mishraraunak <raunakm34@gmail.com>
2024-08-13 22:43:21 +0530

    init

---
+++ librarian/core/prompts.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+from langchain.prompts import PromptTemplate
+
+template = """Create a final answer to the given questions using the provided document excerpts (given in no particular order) as sources. ALWAYS include a "SOURCES" section in your answer citing only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not have enough information to answer the question and leave the SOURCES section empty. Use only the provided documents and do not attempt to fabricate an answer.
+
+QUESTION: {question}
+=========
+{summaries}
+=========
+FINAL ANSWER:"""
+
+STUFF_PROMPT = PromptTemplate(
+    template=template, input_variables=["summaries", "question"]
+)
---
+++ librarian/main.py
@@ -0,0 +1,128 @@
+import streamlit as st
+import os
+
+from librarian.ui import (
+    wrap_doc_in_html,
+    is_query_valid,
+    is_file_valid,
+    is_open_ai_key_valid,
+    display_file_read_error,
+)
+
+from librarian.core.caching import bootstrap_caching
+
+from librarian.core.parsing import read_file
+from librarian.core.chunking import chunk_file
+from librarian.core.embedding import embed_files
+from librarian.core.qa import query_folder
+from librarian.core.utils import get_llm
+
+
+EMBEDDING = "openai"
+VECTOR_STORE = "faiss"
+MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
+
+st.set_page_config(page_title="Librarian", page_icon="📖", layout="wide")
+st.header("Librarian 📖")
+
+# Enable caching for expensive functions
+bootstrap_caching()
+
+st.markdown(
+    "## How to use\n"
+    "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
+    "2. Upload a pdf, docx, or txt file📄\n"
+    "3. Ask a question about the document💬\n"
+)
+
+api_key_input = st.text_input(
+    "OpenAI API Key",
+    type="password",
+    placeholder="Paste your OpenAI API key here (sk-...)",
+    help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
+    value=os.environ.get("OPENAI_API_KEY", None)
+    or st.session_state.get("OPENAI_API_KEY", ""),
+)
+
+st.session_state["OPENAI_API_KEY"] = api_key_input
+
+openai_api_key = st.session_state.get("OPENAI_API_KEY")
+
+if not openai_api_key:
+    st.warning(
+        "Enter your OpenAI API key in the sidebar. You can get a key at"
+        " https://platform.openai.com/account/api-keys."
+    )
+
+
+uploaded_file = st.file_uploader(
+    "Upload a pdf, docx, or txt file",
+    type=["pdf", "docx", "txt"],
+    help="Scanned documents are not supported yet!",
+)
+
+model: str = st.selectbox("Model", options=MODEL_LIST)  # type: ignore
+
+if not uploaded_file:
+    st.stop()
+
+try:
+    file = read_file(uploaded_file)
+except Exception as e:
+    display_file_read_error(e, file_name=uploaded_file.name)
+
+chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
+
+if not is_file_valid(file):
+    st.stop()
+
+
+if not is_open_ai_key_valid(openai_api_key, model):
+    st.stop()
+
+
+with st.spinner("Indexing document... This may take a while⏳"):
+    folder_index = embed_files(
+        files=[chunked_file],
+        embedding=EMBEDDING if model != "debug" else "debug",
+        vector_store=VECTOR_STORE if model != "debug" else "debug",
+        openai_api_key=openai_api_key,
+    )
+
+with st.form(key="qa_form"):
+    query = st.text_area("Ask a question about the document")
+    submit = st.form_submit_button("Submit")
+
+if show_full_doc:
+    with st.expander("Document"):
+        # Hack to get around st.markdown rendering LaTeX
+        st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>",
+                    unsafe_allow_html=True)
+
+
+if submit:
+    if not is_query_valid(query):
+        st.stop()
+
+    # Output Columns
+    answer_col, sources_col = st.columns(2)
+
+    llm = get_llm(model=model, openai_api_key=openai_api_key, temperature=0)
+    result = query_folder(
+        folder_index=folder_index,
+        query=query,
+        return_all=return_all_chunks,
+        llm=llm,
+    )
+
+    with answer_col:
+        st.markdown("#### Answer")
+        st.markdown(result.answer)
+
+    with sources_col:
+        st.markdown("#### Sources")
+        for source in result.sources:
+            st.markdown(source.page_content)
+            st.markdown(source.metadata["source"])
+            st.markdown("---")
+
